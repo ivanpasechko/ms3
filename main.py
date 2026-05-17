e6d2749a8659be8b3a6af48d054a000be1af45f4
ivas <ivas@gmail.com>
2026-05-17 15:47:53 +0300

    убрал .env, переписал main

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+import uvicorn
 from fastapi import FastAPI
 from app.core.database import init_db
 from app.api.routers import api_router
@@ -11,5 +13,11 @@ def on_startup():
 app.include_router(api_router)
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main.py:app", host="0.0.0.0", port=8000, reload=True)
+    # Считываем переменные, задавая значения по умолчанию на случай их отсутствия
+    host = os.getenv("APP_HOST", "0.0.0.0")
+    port = int(os.getenv("APP_PORT", 8000))
+    
+    # Переводим строковое значение из .env в булево (True/False)
+    reload = os.getenv("APP_RELOAD", "True").lower() in ("true", "1", "yes")
+
+    uvicorn.run("main:app", host=host, port=port, reload=reload)
